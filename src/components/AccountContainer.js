a3432ebc6b2245dfe2e19199db514909abde3dd8
arichards4814 <arichards4814@gmail.com>
2020-03-18 10:39:06 -0400

    Search function works

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -9,17 +9,19 @@ class AccountContainer extends Component {
 
   state = {
     transactions: [],
+    filteredTransactions: [],
     date: "",
     description: "",
     category: "",
-    amount: ""
+    amount: "",
+    searchTerm: ""
   }
 
 
   componentDidMount(){
     fetch(baseURL)
       .then(resp => resp.json())
-        .then(body => this.setState({transactions: body}))
+        .then(body => this.setState({transactions: body, filteredTransactions: body}))
   }
 
   addTransaction = (e) => {
@@ -44,6 +46,9 @@ class AccountContainer extends Component {
         let transactionsCopy = [...this.state.transactions]
         transactionsCopy.push(body)
         this.setState({transactions: transactionsCopy})
+        let filteredTransactionsCopy = [...this.state.filteredTransactions]
+        filteredTransactionsCopy.push(body)
+        this.setState({filteredTransactions: filteredTransactionsCopy})
       })
   }
 
@@ -51,12 +56,26 @@ class AccountContainer extends Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
+  handleSearchChange = (e) => {
+    this.setState({searchTerm: e.target.value},
+      () => {
+        if (this.state.searchTerm === ""){
+          this.setState({filteredTransactions: this.state.transactions})
+        } else {
+        let filteredTransactionsCopy2 = [...this.state.filteredTransactions]
+        let newFiltered = filteredTransactionsCopy2.filter(transaction => transaction.description.includes(this.state.searchTerm))
+        this.setState({filteredTransactions: newFiltered})
+        }
+      })
+  }
+
   render() {
+    console.log(this.state.searchTerm)
     return (
       <div>
-        <Search />
+        <Search handleSearchChange={this.handleSearchChange} searchTerm={this.state.searchTerm}/>
         <AddTransactionForm handleSubmit={this.addTransaction} handleChange={this.handleChange}/>
-        <TransactionsList transactions={this.state.transactions}/>
+        <TransactionsList transactions={this.state.filteredTransactions}/>
       </div>
     );
   }

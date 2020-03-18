4bcd183150ab0eb7b738397b3ba6cb02ca5bb617
jhuang429 <jhuang9240@gmail.com>
2020-03-18 10:12:07 -0400

    see transactions complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,27 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+
+const URL = "http://localhost:6001/transactions"
+
 class AccountContainer extends Component {
+
+  state = {
+    trans: []
+  }
+
+  componentDidMount() {
+    fetch(URL).then(resp=>resp.json()).then(content=>
+      this.setState({trans: content})
+    )
+  }
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList trans={this.state.trans}/>
       </div>
     );
   }

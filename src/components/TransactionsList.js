5973bc33de5d2f08c36e69d0ca2fdf0d5017d10c
github username <github email address>
2020-03-18 11:29:49 -0400

    core delivs. sort not working.

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,53 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = (props) => {
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">Date</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Description</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Category</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Amount</h3>
-          </th>
-        </tr>
-        {props.transactions.map(transaction => <Transaction key={transaction.id} transaction={transaction} />)}
-      </tbody>
-    </table>
-  );
+class TransactionsList extends React.Component {
+  state ={
+    sort: ''
+  }
+
+  handleSort = e => {
+    this.setState({sort: e.target.value})
+  }
+
+  runSort = (sortTerm) => {
+    if(sortTerm){
+      const sortedArray = [...this.props.transactions].sort(function(a,b){return a.sortTerm - b.sortTerm})
+      return sortedArray.map(transaction => <Transaction key={transaction.id} transaction={transaction} />)
+    } else {
+      return this.props.transactions.map(transaction => <Transaction key={transaction.id} transaction={transaction} />)
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        Sort by: <select onChange={this.handleSort}>
+          <option>Category</option>
+          <option>Description</option>
+        </select>
+        <table className="ui celled striped padded table">
+          <tbody>
+            <tr>
+              <th>
+                <h3 className="ui center aligned header">Date</h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">Description</h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">Category</h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">Amount</h3>
+              </th>
+            </tr>
+            {this.runSort(this.state.sort)}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 };
 
 export default TransactionsList;

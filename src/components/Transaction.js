e68c662b2fbc6dce5868f2fd86b27b9b13df8f65
enteraether <thetheresagarcia@gmail.com>
2020-03-18 10:26:42 -0400

    finally rendered transactions!

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const Transaction = (props) => {
+
+  //will refactor if i have time
+  // console.log(props.transaction)
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{props.transaction.date}</td>
+      <td>{props.transaction.description}</td>
+      <td>{props.transaction.category}</td>
+      <td>{props.transaction.amount}</td>
     </tr>
   );
 };

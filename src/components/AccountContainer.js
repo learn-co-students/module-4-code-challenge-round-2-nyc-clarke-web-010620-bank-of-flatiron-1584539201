4607a2689ed140e67cddc90fff4e8d3058ca2e2b
github username <github email address>
2020-03-18 10:09:50 -0400

    ugly table

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,25 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const URL = 'http://localhost:6001/transactions'
+
 class AccountContainer extends Component {
+  state = {
+    transactions: []
+  }
+  
+  componentDidMount(){
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(transactions => this.setState({transactions}))
+  }
+  
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }

5bc9e163cbc78237786d8e9e5048dd757f6cd3d0
enteraether <thetheresagarcia@gmail.com>
2020-03-18 10:12:18 -0400

    abt to map through transactions

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,21 @@ import "../stylesheets/App.css";
 class App extends Component {
 
   state = {
-    transactions: []
+    transactions: [],
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
   }
 
   componentDidMount () {
     fetch('http://localhost:6001/transactions')
     .then(r=>r.json())
-    .then(console.log)
+    .then(transactions => {
+      this.setState({
+        transactions: transactions
+      })
+    })
 
   }
 
@@ -21,7 +29,7 @@ class App extends Component {
         <div className="ui segment violet inverted">
           <h2>The Royal Bank of Flatiron</h2>
         </div>
-        <AccountContainer />
+        <AccountContainer transactions={this.state.transactions} />
       </div>
     );
   }

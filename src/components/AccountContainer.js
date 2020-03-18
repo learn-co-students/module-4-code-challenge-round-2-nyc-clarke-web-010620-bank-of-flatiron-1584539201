26e647db61d35c3ee86fa330127ac1d0878785c5
github username <github email address>
2020-03-18 10:42:16 -0400

    functional search but case-dependent

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -7,7 +7,8 @@ const URL = 'http://localhost:6001/transactions'
 
 class AccountContainer extends Component {
   state = {
-    transactions: []
+    transactions: [],
+    searchText: ''
   }
   
   componentDidMount(){
@@ -19,11 +20,19 @@ class AccountContainer extends Component {
   addNewToList = (transaction) => {
     this.setState({transactions: [...this.state.transactions, transaction]})
   }
+
+  handleSearchText = e => {
+    this.setState({searchText: e.target.value})
+  }
+
+  processSearch = () => {
+    this.setState({transactions: [...this.state.transactions.filter(transaction => transaction.description.includes(this.state.searchText))]})
+  }
   
   render() {
     return (
       <div>
-        <Search />
+        <Search handleSearchText={this.handleSearchText} processSearch={this.processSearch}/>
         <AddTransactionForm url={URL} addNewToList={this.addNewToList}/>
         <TransactionsList transactions={this.state.transactions}/>
       </div>

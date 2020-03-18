bdf0a77418f1aec9ad301c0943809a60a7205dd5
Ben Kagan <kagan.benjamin@gmail.com>
2020-03-18 10:17:01 -0400

    deliverable 1 complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+const BASE_URL = `http://localhost:6001/transactions`
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: []
+  }
+
+  componentDidMount() {
+    fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({ transactions: data })
+    })
+  }
+
   render() {
+    console.log(this.state.transactions)
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     );
   }

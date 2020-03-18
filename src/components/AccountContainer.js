48365a48e09b38c13366758b468d04eda1141ca6
Nathan Brickett <natey37@gmail.com>
2020-03-18 10:33:12 -0400

    finished delivarables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -6,7 +6,9 @@ import AddTransactionForm from "./AddTransactionForm";
 class AccountContainer extends Component {
 
   state = {
-    transactions: []
+    transactions: [],
+    filteredTransactions: [],
+    search: ""
   }
 
   componentDidMount(){
@@ -15,17 +17,37 @@ class AccountContainer extends Component {
     .then(resp => resp.json())
     .then(transactions => {
       this.setState({
-        transactions: transactions
+        transactions: transactions,
+        filteredTransactions: transactions
       })
     })
   }
 
+  addNewTransaction = (newTransaction) => {
+    this.setState({
+      transactions: [...this.state.transactions, newTransaction]
+    })
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value
+    }, () => {
+      
+      this.setState({
+        filteredTransactions: this.state.transactions.filter(transaction => transaction.description.includes(this.state.search))
+      })
+      
+    })
+  }
+
   render() {
+    console.log(this.state.search)
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList transactions={this.state.transactions}/>
+        <Search search={this.state.search} handleSearch={this.handleSearch}/>
+        <AddTransactionForm addNewTransaction={this.addNewTransaction}/>
+        <TransactionsList transactions={this.state.filteredTransactions}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -11,27 +11,54 @@ class AddTransactionForm extends Component {
     }
   }
 
+  resetForm = () => {
+    this.setState({
+      form: {
+        date: '',
+        description: '',
+        category: '',
+        amount: ''
+      }
+    })
+  }
+
   handleChange = (event) => {
     this.setState({
       form: {...this.state.form, [event.target.name]: event.target.value}
     })
   }
+
+  handleSubmit = (event, transaction) => {
+    event.preventDefault()
+    fetch('http://localhost:6001/transactions', {
+      method: "POST", 
+      headers: {'Content-Type': 'application/json', 'Accepts': 'application/json'}, 
+      body: JSON.stringify(transaction)
+    }).then(resp => resp.json()).then(newTransaction => {
+      this.props.addNewTransaction(newTransaction)
+      this.resetForm()
+    })
+
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input value={this.state.form.date}type="date" name="date" onChange={this.handleChange}/>
+            <input value={this.state.form.description}type="text" name="description" placeholder="Description" onChange={this.handleChange}/>
+            <input value={this.state.form.category}type="text" name="category" placeholder="Category" onChange={this.handleChange}/>
             <input
+              value={this.state.form.amount}
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.handleChange}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button onClick={(event) => this.handleSubmit(event, this.state.form)} className="ui button" type="submit">
             Add Transaction
           </button>
         </form>

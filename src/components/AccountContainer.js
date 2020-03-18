5d3a562c0c8bc81eec1954e6ef7fcfa3e4641bc4
arichards4814 <arichards4814@gmail.com>
2020-03-18 10:22:10 -0400

    Posts to database and renders to page w/o reloading

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,24 +3,59 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const baseURL = 'http://localhost:6001/transactions'
+
 class AccountContainer extends Component {
 
   state = {
-    transactions: []
+    transactions: [],
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
   }
 
 
   componentDidMount(){
-    fetch('http://localhost:6001/transactions')
+    fetch(baseURL)
       .then(resp => resp.json())
         .then(body => this.setState({transactions: body}))
+  }
+
+  addTransaction = (e) => {
+    e.preventDefault()
+
+    let data = {
+      date: this.state.date,
+      description: this.state.description,
+      category: this.state.category,
+      amount: parseInt(this.state.amount)
+    }
+
+    fetch(baseURL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(resp => resp.json())
+      .then(body => {
+        let transactionsCopy = [...this.state.transactions]
+        transactionsCopy.push(body)
+        this.setState({transactions: transactionsCopy})
+      })
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
+        <AddTransactionForm handleSubmit={this.addTransaction} handleChange={this.handleChange}/>
         <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -4,16 +4,17 @@ class AddTransactionForm extends Component {
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.props.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={this.props.handleChange} />
+            <input type="text" name="description" placeholder="Description" onChange={this.props.handleChange}/>
+            <input type="text" name="category" placeholder="Category" onChange={this.props.handleChange}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.props.handleChange}
             />
           </div>
           <button className="ui button" type="submit">

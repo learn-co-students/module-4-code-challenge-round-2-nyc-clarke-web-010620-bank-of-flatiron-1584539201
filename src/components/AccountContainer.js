677c4164a8cf569a48007e221e56bac507398909
Steven Zhu <stevenzhu03@gmail.com>
2020-03-18 10:18:18 -0400

    rendered all transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,14 +2,36 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import Transaction from "./Transaction";
+
+
+const API = "http://localhost:6001/transactions"
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(transactions => this.setState({
+      transactions
+    }))
+  }
+
+  renderTransactions = () => {
+    return this.state.transactions.map(transaction => <Transaction transaction={transaction}/>)
+  }
+
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList renderTransactions={this.renderTransactions} />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: "",
+    description: "",
+    category: "",
+    amount: null
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date}/>
+            <input type="text" name="description" placeholder="Description" value={this.state.description}/>
+            <input type="text" name="category" placeholder="Category" value={this.state.category} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              value={this.state.amount}
             />
           </div>
           <button className="ui button" type="submit">
